e4281bffc935170be35d8c4d1eae06a79a23afae
Shardul Kulkarni <shardul.kulkarni@equifax.com>
2020-08-21 17:47:41 -0500

    basic springboot

---
+++ src/main/java/com/equifax/ews/qualityhubservice/TestResultParser/TestResultParserApplication.java
@@ -0,0 +1,13 @@
+package com.equifax.ews.qualityhubservice.TestResultParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestResultParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestResultParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equifax/ews/qualityhubservice/TestResultParser/TestResultParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equifax.ews.qualityhubservice.TestResultParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestResultParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
